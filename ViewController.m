fa4fcd271490e2ed11a0c1b1903f9dcaabe828d9
王钱钧 <arthur.wang@yoho.cn>
2014-09-02 18:10:45 +0800

    添加代码

--- ViewController.m
+++ ViewController.m
@@ -5,7 +5,7 @@
 //  Created by 王钱钧 on 14-9-1.
 //  Copyright (c) 2014年 王钱钧. All rights reserved.
 //
-
+// https://developer.apple.com/library/ios/documentation/UserExperience/Conceptual/iPhoneAccessibility/Introduction/Introduction.html
 #import "ViewController.h"
 
 @interface ViewController ()
