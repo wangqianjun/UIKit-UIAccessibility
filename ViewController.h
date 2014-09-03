2716facb4be027497016c55e75f41b3de3cdfb55
王钱钧 <arthur.wang@yoho.cn>
2014-09-03 16:38:27 +0800

    add codes

--- ViewController.h
+++ ViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <UIKit/UIAccessibility.h>
 
 @interface ViewController : UIViewController
 
--- ViewController.m
+++ ViewController.m
@@ -6,9 +6,13 @@
 //  Copyright (c) 2014年 王钱钧. All rights reserved.
 //
 // https://developer.apple.com/library/ios/documentation/UserExperience/Conceptual/iPhoneAccessibility/Introduction/Introduction.html
+
+//http://wenku.baidu.com/view/d1c77a781711cc7931b716d7###
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) NSMutableArray *accessibleElements;
 
 @end
 
@@ -17,6 +21,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self setIsAccessibilityElement:YES];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,4 +32,41 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (NSMutableArray *)accessibleElements
+{
+    if (_accessibleElements != nil) {
+        return _accessibleElements;
+    }
+    
+    _accessibleElements = [[NSMutableArray alloc]init];
+    UIAccessibilityElement *element1 = [[UIAccessibilityElement alloc]initWithAccessibilityContainer:self];
+    [_accessibleElements addObject: element1];
+    UIAccessibilityElement *element2 = [[UIAccessibilityElement alloc]initWithAccessibilityContainer:self];
+    [_accessibleElements addObject:element2];
+    
+    return _accessibleElements;
+    
+}
+
+- (BOOL)isAccessibilityElement
+{
+    return YES;
+}
+
+/* The following methods are implementations of UIAccessibilityContainer protocol methods. */
+- (NSInteger)accessibilityElementCount
+{
+    return [[self accessibleElements] count];
+}
+
+- (id)accessibilityElementAtIndex:(NSInteger)index
+{
+    return [[self accessibleElements] objectAtIndex:index];
+}
+
+- (NSInteger)indexOfAccessibilityElement:(id)element
+{
+    return [[self accessibleElements] indexOfObject:element];
+}
 @end
